83dc71c4d7712dc3cdb0f878835085d85133b420
ericiscoding <ericjwilhite@gmail.com>
2019-08-28 15:32:18 -0700

    Added some more functionality to the CLI class.

--- lib/working_days/cli.rb
+++ lib/working_days/cli.rb
@@ -2,14 +2,28 @@
 
 class WorkingDays::CLI
 
+    attr_reader :year, :country
+
     def initialize
     end
 
     def call
+        puts "Welcome to Working Days!"
+        get_holidays
+        menu
+    end
+
+    def get_holidays
         puts <<~DOC
-        Welcome to Working Days!
         Before we begin, I will need a bit of information from you.
+        What year is it? (ex. 2019)
         DOC
-        
+        @year = gets.strip
+        puts "What country are you from? Please use your country's 2 letter country code. (IE: US, CA, UK)"
+        @country = gets.strip
+        puts "It is #{@year}, and you are from #{@country}."
+    end
+
+    def menu
     end
 end
