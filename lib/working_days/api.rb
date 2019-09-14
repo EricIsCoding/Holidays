dcb88da2b4ae8ccf6c12437ae4fe496eacdd67cf
Eric J Wilhite <EricJWilhite@gmail.com>
2019-09-14 10:26:52 -0700

    Changes the API methods to class methods.
    Prior to this change, you needed to create an instance of the API
    class to use the methods.
    
    This allows the class methods to be called without having to create any
    extra instances of th API.

--- lib/working_days/api.rb
+++ lib/working_days/api.rb
@@ -1,14 +1,14 @@
 class WorkingDays::API
 
-    def get_next(country)
+    def self.get_next(country)
         parse(HTTParty.get("https://date.nager.at/api/v2/nextpublicholidays/#{country}"))
     end
 
-    def get_year(country, year)
+    def self.get_year(country, year)
         parse(HTTParty.get("https://date.nager.at/api/v2/publicholidays/#{year}/#{country}"))
     end
 
-    def parse(response)
+    def self.parse(response)
         @repsonse.each do |holiday|
             WorkingDays::Holiay.new(holiday["name"], holiday["date"], holiday["global"])
         end
