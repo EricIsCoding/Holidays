37978f56fbb2ebc388aa6e0e70750930e218a8a2
Eric J Wilhite <EricJWilhite@gmail.com>
2019-09-14 22:32:00 -0700

    Refactors get_next Prior to this change, get_next wasn't getting the proper list of holidays.
    This makes it so get_next will only parse the hoildays in teh remaining calendar year.

--- lib/holidays/api.rb
+++ lib/holidays/api.rb
@@ -1,7 +1,12 @@
 class Holidays::API
 
-    def self.get_next(country)
-        parse(HTTParty.get("https://date.nager.at/api/v2/nextpublicholidays/#{country}"))
+    def self.get_next(country, year)
+        response = HTTParty.get("https://date.nager.at/api/v2/nextpublicholidays/#{country}")
+        response = response.find_all do |holiday| 
+            date = holiday["date"].split("-")
+            year == date[0].to_i
+        end
+        parse(response)
     end
 
     def self.get_year(country, year)
@@ -9,8 +14,8 @@ class Holidays::API
     end
 
     def self.parse(response)
-        @repsonse.each do |holiday|
-            Holidays::Holiay.new(holiday["name"], holiday["date"], holiday["global"])
+        response.each do |holiday|
+            Holidays::Holiday.new(holiday["name"], holiday["date"], holiday["global"])
         end
     end
 end
